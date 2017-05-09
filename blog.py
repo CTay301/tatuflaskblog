47bfb3476733e05c928bc533538df27dc039ffcd
Corey Taylor <cjtaylor301@gmail.com>
2017-05-09 17:38:47 +0000

    First Commit

---
+++ blog.py
@@ -0,0 +1,93 @@
+# blog.py - controller
+
+
+# imports
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
+import sqlite3
+from functools import wraps
+
+# configuration
+DATABASE = '/home/taylormadeblog/site/blog/blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'HU19'
+
+app = Flask(__name__)
+
+# pulls in configurations by looking for UPPERCASE variables
+app.config.from_object(__name__)
+# for reference: app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/TatuDaGawd/mysite/tmp/database.db'
+
+
+# function used for connecting to the database
+def connect_db():
+     return sqlite3.connect(app.config['DATABASE'])
+
+
+
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or\
+                request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid login. Please try again.'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error), status_code
+
+
+@app.route('/main')
+@login_required
+def main():
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute(
+            'insert into posts (title, post) values (?, ?)',
+            [request.form['title'], request.form['post']]
+        )
+        g.db.commit()
+        g.db.close()
+        flash('Post successful!')
+        return redirect(url_for('main'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+#Was true
+if __name__ == '__main__':
+    app.run(debug=False)
